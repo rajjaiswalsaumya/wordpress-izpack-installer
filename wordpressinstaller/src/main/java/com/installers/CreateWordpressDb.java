ea59c7b69f06ada8e8f5be13c2e9f5e60f1e5d29
ndesk1900 <ndesk1900@gmail.com>
2012-12-11 22:06:44 +0000

    Added more panels.

---
+++ wordpressinstaller/src/main/java/com/installers/CreateWordpressDb.java
@@ -0,0 +1,10 @@
+package com.installers;
+
+import com.izforge.izpack.api.handler.AbstractUIProcessHandler;
+
+public class CreateWordpressDb {
+	
+	public void run(AbstractUIProcessHandler handler, String[] args) {
+	    handler.logOutput("Hello, World!", false);
+	}	
+}
