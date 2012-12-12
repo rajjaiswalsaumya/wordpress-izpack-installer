afdc463b5c82f29335f283537c5d634369a6ade0
ndesk1900@gmail.com <ndesk1900@gmail.com@e4e78bda-2bd7-a44d-df4c-b7aa8c720508>
2012-12-12 10:00:26 +0000

    Added some fields. Closer to end.

--- trunk/wordpressinstaller/src/main/java/com/installers/CreateWordpressDb.java
+++ trunk/wordpressinstaller/src/main/java/com/installers/CreateWordpressDb.java
@@ -2,9 +2,57 @@ package com.installers;
 
 import com.izforge.izpack.api.handler.AbstractUIProcessHandler;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateWordpressDb {
-	
+		
 	public void run(AbstractUIProcessHandler handler, String[] args) {
-	    handler.logOutput("Hello, World!", false);
+		
+		String host = args[0];
+		String dbname = args[1];
+		String user = args[2];
+		String pass = args[3];
+		
+		handler.logOutput("Attempting to create DB " + dbname + " on " 
+											+ host + " with user:" + user + 
+														" pass:" + pass, false);
+		
+		Connection conn = null;
+		Statement stat = null;
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://"+host, user, pass);
+			stat = conn.createStatement();
+			stat.executeUpdate("create database " + dbname);
+			handler.logOutput("Created database " + dbname + " on " + host, false);
+
+	    } catch (Exception e) {
+
+	    	handler.logOutput("Couldnt create database for wordpress!", false);
+
+	    } finally {
+	    	
+	    	if ( conn != null ) {
+	    		try {
+					conn.close();
+				} catch (SQLException e) {
+			    	handler.logOutput("Couldnt close DB Connection", false);
+				}
+	    	}
+	    	if ( stat != null ) {
+	    		try {
+					stat.close();
+				} catch (SQLException e) {
+					handler.logOutput("Couldnt close Statement", false);
+				}
+	    	}
+	    	
+	    }
+
+		
 	}	
 }
